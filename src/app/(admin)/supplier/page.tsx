48773679b039302c9a5e3eba29be3d9c10bc77ca
fernando2523 <74076122+fernando2523@users.noreply.github.com>
2023-12-06 00:03:20 +0700

    pushss

--- src/app/(admin)/supplier/page.tsx
+++ src/app/(admin)/supplier/page.tsx
@@ -29,18 +29,37 @@ import { Textarea } from "@/components/ui/textarea"
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-
-
 export default function Supplier() {
 
   const [datasupplier, setdatasupplier]: any = useState([]);
-
+  const [open, setopen]: any = useState(false);
   const [v_supplier, setv_supplier]: any = useState('');
   const [v_contact, setv_contact]: any = useState('');
   const [v_address, setv_address]: any = useState('');
 
   async function savesupplier() {
-    console.log("klick")
+    console.log(v_supplier)
+    console.log(v_contact)
+    console.log(v_address)
+
+    await axios({
+      method: 'post',
+      url: 'http://139.180.130.182:4000/supplier',
+      data: {
+        supplier: v_supplier,
+        contact: v_contact,
+        alamat: v_address
+      }
+    })
+      .then(function (response) {
+        // handle success
+        loadatasupplier()
+        setopen(false)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
   }
 
   async function loadatasupplier() {
@@ -56,7 +75,6 @@ export default function Supplier() {
         // handle error
         console.log(error);
       })
-    console.log(datasupplier)
   }
 
   useEffect(() => {
@@ -73,8 +91,9 @@ export default function Supplier() {
         <div className="font-bold text-4xl">
           <Input type="text" className='w-[400px] shadow-md' placeholder="Search Supplier.." />
         </div>
+
         <div className="absolute right-5">
-          <AlertDialog>
+          <AlertDialog open={open} onOpenChange={setopen}>
             <AlertDialogTrigger asChild>
               <Button variant="outline" className='bg-black text-white font-bold hover:bg-gray-200'>Add New</Button>
             </AlertDialogTrigger>
@@ -114,7 +133,7 @@ export default function Supplier() {
           </AlertDialog>
         </div>
       </div>
-
+      {JSON.stringify(datasupplier)}
       <div className="mt-5 shadow-md">
         <Table className='border border-black-500'>
           <TableHeader className='bg-gray-700'>
@@ -122,6 +141,7 @@ export default function Supplier() {
               <TableHead className="border text-center font-bold text-white w-[50px]">No</TableHead>
               <TableHead className="border text-center font-bold text-white w-[150px]">ID Supplier</TableHead>
               <TableHead className='border text-left font-bold text-white'>Name</TableHead>
+              <TableHead className='border text-left font-bold text-white'>Contact</TableHead>
               <TableHead className="border text-left font-bold text-white">Address</TableHead>
               <TableHead className="border  w-[100px] font-bold text-white">Act</TableHead>
             </TableRow>
@@ -130,7 +150,68 @@ export default function Supplier() {
             {datasupplier.map((dataisi: any, index: any) => (
               <TableRow key={index}>
                 <TableCell className="border text-center w-[50px] font-bold">{index + 1}</TableCell>
-                <TableCell className="border text-center w-[50px] font-bold">{dataisi.id_sup}</TableCell>
+                <TableCell className="border text-center w-[50px]">{dataisi.id_sup}</TableCell>
+                <TableCell className="border text-center w-[50px]">{dataisi.supplier}</TableCell>
+                <TableCell className="border text-center w-[50px]">{dataisi.contact}</TableCell>
+                <TableCell className="border text-center w-[50px]">{dataisi.alamat}</TableCell>
+                <TableCell className="border w-[50px]">
+
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant="link" className=' text-white font-bold hover:bg-gray-200'> <Icon.FileEdit color="#00b3ff" /></Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className='w-[600px]'>
+                      <AlertDialogHeader className='border-b pb-4'>
+                        <AlertDialogTitle >Edit Supplier</AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <div className='flex flex-row text-center mt-2 items-center'>
+                        <div className='basis-1/4 font-bold text-center'>
+                          Supplier :
+                        </div>
+                        <div className='basis-3/4'>
+                          <Input type="text" placeholder="Supplier.." />
+                        </div>
+                      </div>
+                      <div className='flex flex-row text-center mt-2 items-center'>
+                        <div className='basis-1/4 font-bold text-center'>
+                          Contact :
+                        </div>
+                        <div className='basis-3/4'>
+                          <Input type="text" placeholder="Contact.." />
+                        </div>
+                      </div>
+                      <div className='flex flex-row text-center mt-2 items-top'>
+                        <div className='basis-1/4 font-bold text-center'>
+                          Address :
+                        </div>
+                        <div className='basis-3/4'>
+                          <Textarea className='border w-full' placeholder="Address.." />
+                        </div>
+                      </div>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel className='bg-red-400'>Cancel</AlertDialogCancel>
+                        <Button >Save</Button>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant="link" className=' text-white font-bold hover:bg-gray-200'> <Icon.XCircle color="#ff0000" /></Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className='w-[600px]'>
+                      <AlertDialogHeader className='border-b pb-4'>
+                        <AlertDialogTitle >Delet Supplier</AlertDialogTitle>
+                      </AlertDialogHeader>
+
+                      <AlertDialogFooter>
+                        <AlertDialogCancel >Cancel</AlertDialogCancel>
+                        <Button className='bg-red-400 font-bold'>Delete</Button>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
